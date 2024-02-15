18f13b4ac08e12796e3f7c8551a0e936a1d60a05
dh1010a <dh1010a@naver.com>
2024-02-15 16:30:49 +0900

    feat: aws에 서버 업로드

--- src/main/java/com/pickpick/server/global/config/FirebaseConfig.java
+++ src/main/java/com/pickpick/server/global/config/FirebaseConfig.java
@@ -4,11 +4,15 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.messaging.FirebaseMessaging;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 
 @Configuration
 public class FirebaseConfig {
@@ -29,8 +33,10 @@ public class FirebaseConfig {
     FirebaseMessaging firebaseMessaging() throws IOException{
         //ClassPathResource resource = new ClassPathResource("/src/main/resources/picpick-1947c-firebase-adminsdk-oa23k-d93284724e.json");
         //InputStream refreshToken = resource.getInputStream();
-        FileInputStream refreshToken = new FileInputStream("C:\\Users\\dh101\\Desktop\\도현\\대학\\UMC\\pickpick\\src\\main\\resources\\picpick-1947c-firebase-adminsdk-oa23k-d93284724e.json");
-
+        String localPath = "src\\main\\resources\\picpick-1947c-firebase-adminsdk-oa23k-d93284724e.json";
+        String serverPath = "/home/ubuntu/json/picpick-1947c-firebase-adminsdk-oa23k-d93284724e.json";
+//        FileInputStream refreshToken = new FileInputStream(localPath);
+        InputStream refreshToken = loadFirebaseJsonAsInputStream();
         FirebaseApp firebaseApp = null;
         List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
 
@@ -48,5 +54,14 @@ public class FirebaseConfig {
             firebaseApp = FirebaseApp.initializeApp(options);
         }
         return FirebaseMessaging.getInstance(firebaseApp);
+    }
+
+    private InputStream loadFirebaseJsonAsInputStream() throws IOException {
+        // 'src/main/resources' 디렉토리를 기준으로 경로 지정
+        String firebaseJsonPath = "picpick-1947c-firebase-adminsdk-oa23k-d93284724e.json";
+
+        // 리소스를 InputStream으로로드
+        Resource resource = new ClassPathResource(firebaseJsonPath);
+        return resource.getInputStream();
     }
 }
